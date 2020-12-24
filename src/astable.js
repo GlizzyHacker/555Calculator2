393f75f4487f74000cedde1c857e2b666cda45b2
GlizzyHacker <75539035+GlizzyHacker@users.noreply.github.com>
2020-12-24 20:39:19 +0100

    ???

--- src/astable.js
+++ src/astable.js
@@ -143,16 +143,6 @@ class Field {
     }
   }
 }
-var Fields = [
-  new Field("Capacitor", "c", false, Capacitor),
-  new Field("Resistor 1", "r1", false, Resistor),
-  new Field("Resistor 2", "r2", false, Resistor),
-  new Field("Frequency", "f", true, Frequency),
-  new Field("Duty Cycle", "dc", true),
-  new Field("Time", "t", true, Time),
-  new Field("Time High", "th", true, Time),
-  new Field("Time Low", "tl", true, Time)
-];
 
 function GetField(id) {
   for (var field in Fields) {
