62a2055518c70c05ea506ada3bb3967e13130a4a
GlizzyHacker <75539035+GlizzyHacker@users.noreply.github.com>
2020-12-25 16:10:22 +0100

    Astable2

--- src/astable2.js
+++ src/astable2.js
@@ -20,36 +20,53 @@ var p1 = document.createElement("p");
 p1.innerHTML = "Please select a component you know the value of";
 p1.className = "center";
 
-var body2 = document.createElement("div");
+var body2 = document.createElement("form");
 
-var buttonC = document.createElement("button");
+var buttonC = document.createElement("input");
+buttonC.type = "radio";
 buttonC.innerHTML = "Capacitor";
+buttonC.name = "comp";
 buttonC.onclick = function () {
   Fields[2].ReadWrite();
   Fields[3].ReadOnly();
   Fields[4].ReadOnly();
 };
-var buttonR1 = document.createElement("button");
+var l1 = document.createElement("label");
+l1.innerHTML = "Capacitor";
+l1.for = "cap";
+var buttonR1 = document.createElement("input");
+buttonR1.type = "radio";
 buttonR1.innerHTML = "Resistor 1";
+buttonR1.name = "comp";
 buttonR1.onclick = function () {
   Fields[3].ReadWrite();
   Fields[2].ReadOnly();
   Fields[4].ReadOnly();
 };
-var buttonR2 = document.createElement("button");
+var l2 = document.createElement("label");
+l2.innerHTML = "Resistor 1";
+l2.for = "res1";
+var buttonR2 = document.createElement("input");
+buttonR2.type = "radio";
 buttonR2.innerHTML = "Resistor 2";
+buttonR2.name = "comp";
 buttonR2.onclick = function () {
   Fields[4].ReadWrite();
   Fields[2].ReadOnly();
   Fields[3].ReadOnly();
 };
+var l3 = document.createElement("label");
+l3.innerHTML = "Resistor 2";
+l3.for = "res2";
 
 body.append(p1);
 body.append(body2);
 body2.append(buttonC);
+body2.append(l1);
 body2.append(buttonR1);
+body2.append(l2);
 body2.append(buttonR2);
-
+body2.append(l3);
 var Fields = [
   new Field("Frequency", "f", false, Frequency),
   new Field("Duty Cycle", "dc", false, Percent),
